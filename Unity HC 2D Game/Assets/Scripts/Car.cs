5e6a11bb0cd8cc0037690404019c599a0f363bbf
BBYuJi <74964675+BBYuJi@users.noreply.github.com>
2020-12-08 21:37:01 +0800

    事件設定

--- Unity HC 2D Game/Assets/Scripts/Car.cs
+++ Unity HC 2D Game/Assets/Scripts/Car.cs
@@ -3,12 +3,64 @@
 public class Car : MonoBehaviour
 {
     // 註解 (說明)
-    // 類別
+    // 數值
     // 語法
-    // 名稱； 
+    // 類別 名稱； 
 
-    int number;
+
+    //私人 private 不會顯示在面板上
+    //公開 public  會顯示在面板上
+
+    [Header ("這是車子的尺寸"), Range (50,200)] 
+    public int size = 200;
+    [Tooltip ("這是車子的重量，單位是噸"), Range (1.5f,3f)]
+    public float weight = 1.5f;
+    [Header ("品牌"),Tooltip ("車子的品牌")]
+    public string brand = "BMW";
+    [Header("是否有天窗"), Tooltip("車子有無天窗")]
+    public bool haswindow = true;
+
+    
+    //顏色 
     
 
+    public Color colorA = Color.black;
+    public Color colorB = new Color(0.5f, 2, 1);
+    public Color colorC = new Color(0.5f, 2, 1,0.5f);
+
+    //向量 Vector2
+
+    public Vector2 V2A = Vector2.zero;
+    public Vector2 V2B = Vector2.one;
+
+    //音效與圖片
+
+    public AudioClip sound1;
+    public Sprite sprite;
+
+    //遊戲物件與預製物
+
+    public GameObject goA;
+    public GameObject goB;
+
+    //元件:屬性面板上的粗體字
+
+    public Transform trA;
+    public Camera cma;
+
+    private void Start()
+    {
+        print("哈囉,沃德");
+
+        //取得欄位(抓出資料)
+
+        print(size);
+        print(brand);
+
+        //設定欄位(修改)
+
+        haswindow = false;
+
+    }
 
 }
---
+++ Unity HC 2D Game/Assets/Scripts/TetrisManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class TetrisManager : MonoBehaviour
+{
+    [Header("掉落時間"),Range(0.1f,3)]
+    public float Droptime = 1.5f;
+    [Header("目前分數")]
+    public int CurrentScore;
+    [Header("最高時間")]
+    public int Scorehight;
+    [Header("等級")]
+    public int LV = 1;
+    [Header("結束畫面")]
+    public GameObject End;
+    [Header("音效:掉落，移動，消除，結束。")]
+    public AudioClip soundDrop;
+    public AudioClip soundMove;
+    public AudioClip soundEliminate;
+    public AudioClip soundGmEnd;
+
+
+}
